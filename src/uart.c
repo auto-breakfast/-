8649d0bf8b12d35c8bb32b7361df4b6f1ab7a8b6
1105192112 <45084888+1105192112@users.noreply.github.com>
2018-12-13 19:41:27 +0800

    Add files via upload

--- src/uart.c
+++ src/uart.c
@@ -9,18 +9,22 @@
 #include <xc.h>
 
 void initalUART(void) {
+    
     RCSTAbits.SPEN=0;//關
     
-    TXSTA=0x0000;
+    TRISC=0x00;
+    TRISCbits.RC6=1;
+    TRISCbits.RC7=1;
+    
+    TXSTA=0x00;
     TXSTAbits.TX9=0;//9bit
     TXSTAbits.TXEN=1;//TX開啟
     TXSTAbits.SYNC=0;//模式選擇 0為異步 1為同步
     TXSTAbits.SENDB=0;
     TXSTAbits.BRGH=0;//鮑率選擇
-    TXSTAbits.TRMT=0;
     TXSTAbits.TX9D=0;
     
-    RCSTA=0x0000;
+    RCSTA=0x00;
     RCSTAbits.RX9=0;
     RCSTAbits.CREN=1;
     RCSTAbits.FERR=0;
@@ -28,11 +32,14 @@ void initalUART(void) {
     RCSTAbits.RX9D=0;
     
     
-    BAUDCON=0x0000;
+    BAUDCON=0x00;
     BAUDCONbits.ABDOVF=0;
     BAUDCONbits.BRG16=0;
     
     SPBRG=64;
+    
+    PIE1bits.RCIE=0;
+    
     RCSTAbits.SPEN=1;//開
 }
 
