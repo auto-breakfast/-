b5ef61b6e9c211e0a286851e269a4444bab6d356
h51679438 <36026350+h51679438@users.noreply.github.com>
2018-12-21 08:01:41 +0800

    Add files via upload

---
+++ src/Seven-segment display.c
@@ -0,0 +1,48 @@
+/*
+ * File:   Seven-segment display.c
+ * Author: User
+ *
+ * Created on 2018年12月7日, 上午 9:53
+ */
+
+
+#include "mydelay.h"
+#include "Seven-segment display.h"
+#include "defines.h"
+void Display(int hour,int minute)
+{
+    int row;
+    int i=0;
+    TRISD = 0x00;
+    TRISC &= 0x10;
+    int str[10]={0x3F,0x06,0x5B,0x4F,0x66,0x6C,0x7C,0x07,0x7F,0x6F};
+    
+    for (i=3;i>=0;i--)
+    {
+        switch(i) {
+            case 0:
+                LATCbits.LATC3 = 1;
+                LATCbits.LATC0 = 0;
+                LATD = ~str[hour/10];
+                break;
+            case 1:
+                LATCbits.LATC2 = 1;
+                LATCbits.LATC3 = 0;
+                LATD = ~str[hour%10];
+                break;
+            case 2:
+                LATCbits.LATC1 = 1;
+                LATCbits.LATC2 = 0;
+                LATD = ~str[minute/10];
+                break;
+            case 3:
+                LATCbits.LATC0 = 1;
+                LATCbits.LATC1 = 0;
+                LATD = ~str[minute%10];
+                break;
+            default:
+                break;
+        }
+                    
+    }
+}
---
+++ src/Seven-segment display.h
@@ -0,0 +1,11 @@
+
+ 
+#ifndef Seven_segment_display_H
+#define	Seven_segment_display_H
+
+#include "defines.h"
+void Display(int hour,int minute);
+
+
+#endif	/* XC_HEADER_TEMPLATE_H */
+
